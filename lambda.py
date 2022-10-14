8d6de3fdd68310f8e186155fb067c08958840642
vasanth0195 <51111260+vasanth0195@users.noreply.github.com>
2022-10-14 15:33:03 +0530

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -1,16 +1,8 @@
+import json
 import boto3
 import logging
 
-import json
-import access_keys
-
-
-
-
-Access_key = access_keys.access_key_id
-secret_key = access_keys.secret_access_key
-
-IOT_CORE_REGION='us-east-1'
+IOT_CORE_REGION='ap-southeast-1'
 logger_aws_iot_core = logging.getLogger('example_logger')
 logger_aws_iot_core.setLevel(logging.INFO)
 
@@ -20,12 +12,7 @@ pageSize = 2
 
 print('device registration is started')
 # Obtain the IoT Client.
-iot_client = boto3.client('iot', region_name ='us-east-1',
-                            aws_access_key_id = Access_key,
-                            aws_secret_access_key = secret_key)
-
-
-
+iot_client = boto3.client('iot')
 
 
 def aws_iot_core_get_all_things(detail=False):
@@ -134,105 +121,141 @@ def aws_iot_core_get_all_thing_types(detail=False):
 
 
 
-
-
-
-
-
-# main code
-thing_type_name = 'ConnectedBulbs2'
-thing_type_description = 'My Connected Bulbs'
-device_name='Bulb002'
-policyName='testing'
-thingARN=''
-thing_group='testing_group'
-
-thingTypes =aws_iot_core_get_all_thing_types(detail=False)
-
-# print(thingTypes)
-
-if(thing_type_name in thingTypes["thingTypeNames"]):
-        print('thing type already exisist')
-else:
-    iot_client.create_thing_type(
-        thingTypeName = thing_type_name,
-        thingTypeProperties = {
-        'thingTypeDescription': thing_type_description,
-        'searchableAttributes': [
-            'manufacturer',
-            'serial_number',
-            'production_date'
-            ]
-         }
-        )
-    
-
-thingNames = aws_iot_core_get_all_things(detail=False)
-
-
-if(device_name in thingNames["thingNames"]):
-        print(str(device_name)+' thing already exisist')
-else:
-    register_thing=iot_client.create_thing(
-    thingName = device_name,
-    thingTypeName = thing_type_name,
-    attributePayload = {
-        'attributes': {
-            'manufacturer': "GreatCorp",
-            'serial_number': "1.2.3.4.5",
-            'production_date': "2018.02"
+def lambda_handler(event, context):
+    # print(event)
+    if event['headers']['x-api-key'] == 'F77_iot_device_registration':
+        # TODO implement
+        # main code
+        thing_type_name = 'ConnectedBulbs2'
+        thing_type_description = 'My Connected Bulbs'
+        
+        
+        data= event['body']
+        
+        print(data)
+        data1=json.loads(data)
+        
+        
+        
+        device_name=data1['thingName']
+        
+        
+        print(device_name)
+        manufacturer=data1['attributes']['manufacturer']
+        serial_number=data1['attributes']['serial_number']
+        production_date=data1['attributes']['production_date']
+        
+        print(manufacturer)
+        print(serial_number)
+        print(production_date)
+        
+        
+        policyName='testing'
+        thingARN=''
+        thing_group='testing_group'
+        
+        thingTypes =aws_iot_core_get_all_thing_types(detail=False)
+        
+        # print(thingTypes)
+        
+        if(thing_type_name in thingTypes["thingTypeNames"]):
+                print('thing type already exisist')
+        else:
+            iot_client.create_thing_type(
+                thingTypeName = thing_type_name,
+                thingTypeProperties = {
+                'thingTypeDescription': thing_type_description,
+                'searchableAttributes': [
+                    'manufacturer',
+                    'serial_number',
+                    'production_date'
+                    ]
+                 }
+                )
+            
+        
+        thingNames = aws_iot_core_get_all_things(detail=False)
+        
+        
+        if(device_name in thingNames["thingNames"]):
+                print(str(device_name)+' thing already exisist')
+                response=str(device_name)+' thing already exisist'
+                return {
+                'statusCode': 200,
+                'body': json.dumps(response)
+                    }
+                
+                
+        else:
+            register_thing=iot_client.create_thing(
+            thingName = device_name,
+            thingTypeName = thing_type_name,
+            attributePayload =  {
+            'attributes': {
+            'manufacturer': manufacturer,
+            'serial_number': serial_number,
+            'production_date': production_date
             }
-        }
-    )   
-    # add to the thing group
-    thingARN=register_thing['thingArn']
-
-    response = iot_client.add_thing_to_thing_group(
-    thingGroupName=thing_group,
-    thingGroupArn='arn:aws:iot:us-east-1:268152576851:thinggroup/testing_group',
-    thingName=device_name,
-    thingArn=thingARN,
-    overrideDynamicGroups=True
-    )
+            }
+            )   
+        
+            # add to the thing group
+            thingARN=register_thing['thingArn']
+        
+            response = iot_client.add_thing_to_thing_group(
+            thingGroupName=thing_group,
+            thingGroupArn='arn:aws:iot:ap-southeast-1:484265082862:thinggroup/testing_group',
+            thingName=device_name,
+            thingArn=thingARN,
+            overrideDynamicGroups=True
+            )
+            
+        
+            # Create keys and certificates
+            response = iot_client.create_keys_and_certificate(setAsActive=True)
+            print(response)
+            # Get the certificate and key contents
+            certificateArn = response["certificateArn"]
+            certificate = response["certificatePem"]
+            key_public = response["keyPair"]["PublicKey"]
+            key_private = response["keyPair"]["PrivateKey"]
+        
+            # log information
+            logger_aws_iot_core.info(
+                f"\t\tCreating the certificate...")
+        
+            # # Storing the private key
+            # f = open("C:/Users/CloudThat/Documents/vsc-python/AWS IOT/AWS IOT with Lambda/keys/private/"+device_name+"_private.pem.key", "w")
+            # f.write(key_private)
+            # f.close()
+        
+            # # Storing the public key
+            # f = open("C:/Users/CloudThat/Documents/vsc-python/AWS IOT/AWS IOT with Lambda/keys/publick/"+device_name+"_publick.pem.key", "w")
+            # f.write(key_public)
+            # f.close()
+        
+            # # Storing the certificate
+            # f = open("C:/Users/CloudThat/Documents/vsc-python/AWS IOT/AWS IOT with Lambda/keys/certificate/"+device_name+"_certificate.pem.crt", "w")
+            # f.write(certificate)
+            # f.close()
+            
+            # Attach certificate to things
+            iot_client.attach_thing_principal(
+                thingName=device_name, principal=certificateArn)
+        
+            logger_aws_iot_core.info(
+                    f"\tAttaching thing {thingNames} and certificate {certificateArn}...")
+        
+            # Attach policy to things
+            logger_aws_iot_core.info(
+                    f"\tAttaching thing {thingNames} and policy {policyName}...")
+            iot_client.attach_principal_policy(
+                policyName=policyName, principal=certificateArn)
+            
+            return {
+                'statusCode': 200,
+                'body': json.dumps(response)
+            }
+        
+        
     
-
-    # Create keys and certificates
-    response = iot_client.create_keys_and_certificate(setAsActive=True)
-    print(response)
-    # Get the certificate and key contents
-    certificateArn = response["certificateArn"]
-    certificate = response["certificatePem"]
-    key_public = response["keyPair"]["PublicKey"]
-    key_private = response["keyPair"]["PrivateKey"]
-
-    # log information
-    logger_aws_iot_core.info(
-        f"\t\tCreating the certificate...")
-
-    # Storing the private key
-    f = open("C:/Users/CloudThat/Documents/vsc-python/AWS IOT/AWS IOT with Lambda/keys/private/"+device_name+"_private.pem.key", "w")
-    f.write(key_private)
-    f.close()
-
-    # Storing the public key
-    f = open("C:/Users/CloudThat/Documents/vsc-python/AWS IOT/AWS IOT with Lambda/keys/publick/"+device_name+"_publick.pem.key", "w")
-    f.write(key_public)
-    f.close()
-
-    # Storing the certificate
-    f = open("C:/Users/CloudThat/Documents/vsc-python/AWS IOT/AWS IOT with Lambda/keys/certificate/"+device_name+"_certificate.pem.crt", "w")
-    f.write(certificate)
-    f.close()
-    
-    # Attach certificate to things
-    iot_client.attach_thing_principal(
-        thingName=device_name, principal=certificateArn)
-
-    logger_aws_iot_core.info(
-            f"\tAttaching thing {thingNames} and certificate {certificateArn}...")
-
-    # Attach policy to things
-    logger_aws_iot_core.info(
-            f"\tAttaching thing {thingNames} and policy {policyName}...")
-    iot_client.attach_principal_policy(
-        policyName=policyName, principal=certificateArn)
