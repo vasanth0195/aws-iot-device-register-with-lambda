ac48fc38a75a0865dd6f0c169db188137a8b624d
vasanth0195 <51111260+vasanth0195@users.noreply.github.com>
2022-10-31 13:24:01 +0530

    updated

--- device_registration_storekeyin_s3_and_meta_data_in_dynamodb.py
+++ device_registration_storekeyin_s3_and_meta_data_in_dynamodb.py
@@ -13,58 +13,59 @@ pageSize = 2
 print('device registration is started')
 # Obtain the IoT Client.
 iot_client = boto3.client('iot')
+dynamodb_client = boto3.client('dynamodb')
+s3 = boto3.client('s3')
 
+# def aws_iot_core_get_all_things(detail=False):
+#     """
+#     returns all the things registered in the aws-iot-core
+#     """
 
-def aws_iot_core_get_all_things(detail=False):
-    """
-    returns all the things registered in the aws-iot-core
-    """
+#     # Return parameters
+#     thingNames = []
+#     thingArns = []
 
-    # Return parameters
-    thingNames = []
-    thingArns = []
+#     # Create client
+#     # iot_client = boto3.client('iot', IOT_CORE_REGION)
 
-    # Create client
-    iot_client = boto3.client('iot', IOT_CORE_REGION)
+#     # Parameters used to count things and search pages
+#     things_count = 0
+#     page_count = 0
 
-    # Parameters used to count things and search pages
-    things_count = 0
-    page_count = 0
+#     # Log Info
+#     if(detail):
+#         logger_aws_iot_core.info("Getting things")
 
-    # Log Info
-    if(detail):
-        logger_aws_iot_core.info("Getting things")
+#     # Send the first request
+#     response = iot_client.list_things(maxResults=pageSize)
 
-    # Send the first request
-    response = iot_client.list_things(maxResults=pageSize)
+#     # Count the number of the things until no more things are present on the search pages
+#     while(1):
+#         # Increment thing count
+#         things_count = things_count + len(response['things'])
+#         if(detail):
+#             logger_aws_iot_core.info(
+#                 f"\t{len(response['things'])} things are found on the {page_count+1}. page. Checking the next page ...")
 
-    # Count the number of the things until no more things are present on the search pages
-    while(1):
-        # Increment thing count
-        things_count = things_count + len(response['things'])
-        if(detail):
-            logger_aws_iot_core.info(
-                f"\t{len(response['things'])} things are found on the {page_count+1}. page. Checking the next page ...")
+#         # Append found things to the lists
+#         for thing in response['things']:
+#             thingArns.append(thing['thingArn'])
+#             thingNames.append(thing['thingName'])
 
-        # Append found things to the lists
-        for thing in response['things']:
-            thingArns.append(thing['thingArn'])
-            thingNames.append(thing['thingName'])
+#         # Increment Page number
+#         page_count += 1
 
-        # Increment Page number
-        page_count += 1
+#         # Check if nextToken is present for next search pages
+#         if("nextToken" in response):
+#             response = iot_client.list_things(
+#                 maxResults=pageSize, nextToken=response["nextToken"])
+#         else:
+#             break
 
-        # Check if nextToken is present for next search pages
-        if("nextToken" in response):
-            response = iot_client.list_things(
-                maxResults=pageSize, nextToken=response["nextToken"])
-        else:
-            break
-
-    if(detail):
-        logger_aws_iot_core.info(
-            f"\tGetting things is completed. In total {things_count} things are found.")
-    return {"thingArns": thingArns, "thingNames": thingNames}
+#     if(detail):
+#         logger_aws_iot_core.info(
+#             f"\tGetting things is completed. In total {things_count} things are found.")
+#     return {"thingArns": thingArns, "thingNames": thingNames}
 
 
 
@@ -78,7 +79,7 @@ def aws_iot_core_get_all_thing_types(detail=False):
     thingTypeArns = []
 
     # Create client
-    iot_client = boto3.client('iot', IOT_CORE_REGION)
+    # iot_client = boto3.client('iot', IOT_CORE_REGION)
 
     # Parameter used to count thingType names
     types_count = 0
@@ -138,55 +139,66 @@ def lambda_handler(event, context):
         
         
         device_name=data1['thingName']
-        
-        
         print(device_name)
-        manufacturer=data1['attributes']['manufacturer']
-        serial_number=data1['attributes']['serial_number']
-        production_date=data1['attributes']['production_date']
-        
-        print(manufacturer)
-        print(serial_number)
-        print(production_date)
-        
-        
-        policyName='testing'
-        thingARN=''
-        thing_group='testing_group'
-        
-        thingTypes =aws_iot_core_get_all_thing_types(detail=False)
-        
-        # print(thingTypes)
-        
-        if(thing_type_name in thingTypes["thingTypeNames"]):
-                print('thing type already exisist')
-        else:
-            iot_client.create_thing_type(
-                thingTypeName = thing_type_name,
-                thingTypeProperties = {
-                'thingTypeDescription': thing_type_description,
-                'searchableAttributes': [
-                    'manufacturer',
-                    'serial_number',
-                    'production_date'
-                    ]
-                 }
-                )
-            
-        
-        thingNames = aws_iot_core_get_all_things(detail=False)
-        
-        
-        if(device_name in thingNames["thingNames"]):
-                print(str(device_name)+' thing already exisist')
-                response=str(device_name)+' thing already exisist'
-                return {
+        try:
+            response = iot_client.describe_thing(
+                    thingName=device_name
+                    )
+            print("after thing description search")
+            response=str(device_name)+' thing already exisist'
+            return {
                 'statusCode': 200,
                 'body': json.dumps(response)
                     }
+        except:
+            print("This is new resources so we need to create the this device")
+        
+            manufacturer=data1['attributes']['manufacturer']
+            serial_number=data1['attributes']['serial_number']
+            production_date=data1['attributes']['production_date']
+            
+            print(manufacturer)
+            print(serial_number)
+            print(production_date)
+            
+            
+            policyName='testing'
+            thingARN=''
+            thing_group='testing_group'
+            
+            thingTypes =aws_iot_core_get_all_thing_types(detail=False)
+            
+            # print(thingTypes)
+            if(thing_type_name in thingTypes["thingTypeNames"]):
+                    print('thing type already exisist')
+            else:
+                iot_client.create_thing_type(
+                    thingTypeName = thing_type_name,
+                    thingTypeProperties = {
+                    'thingTypeDescription': thing_type_description,
+                    'searchableAttributes': [
+                        'manufacturer',
+                        'serial_number',
+                        'production_date'
+                        ]
+                     }
+                    )
                 
-                
-        else:
+            
+            # thingNames = aws_iot_core_get_all_things(detail=False)
+            
+            
+            # if(device_name in thingNames["thingNames"]):
+            #         print(str(device_name)+' thing already exisist')
+            #         response=str(device_name)+' thing already exisist'
+            #         return {
+            #         'statusCode': 200,
+            #         'body': json.dumps(response)
+            #             }
+                    
+                    
+            # else:
+              
             register_thing=iot_client.create_thing(
             thingName = device_name,
             thingTypeName = thing_type_name,
@@ -224,7 +236,7 @@ def lambda_handler(event, context):
             logger_aws_iot_core.info(
                 f"\t\tCreating the certificate...")
                 
-            s3 = boto3.client('s3')
+            
             bucket_name = "check-bick-iot-device-cetrs"
             directory_name = "my/key/"+device_name+"/"+device_name+"_private.pem.key" #it's name of your folders
             
@@ -239,39 +251,25 @@ def lambda_handler(event, context):
             object = s3.put_object(Body=certificate,Bucket=bucket_name, Key=directory_name)
            
            
-            # # Storing the private key
-            # f = open("C:/Users/CloudThat/Documents/vsc-python/AWS IOT/AWS IOT with Lambda/keys/private/"+device_name+"_private.pem.key", "w")
-            # f.write(key_private)
-            # f.close()
-        
-            # # Storing the public key
-            # f = open("C:/Users/CloudThat/Documents/vsc-python/AWS IOT/AWS IOT with Lambda/keys/publick/"+device_name+"_publick.pem.key", "w")
-            # f.write(key_public)
-            # f.close()
-        
-            # # Storing the certificate
-            # f = open("C:/Users/CloudThat/Documents/vsc-python/AWS IOT/AWS IOT with Lambda/keys/certificate/"+device_name+"_certificate.pem.crt", "w")
-            # f.write(certificate)
-            # f.close()
             
             # Attach certificate to things
             iot_client.attach_thing_principal(
                 thingName=device_name, principal=certificateArn)
         
             logger_aws_iot_core.info(
-                    f"\tAttaching thing {thingNames} and certificate {certificateArn}...")
+                    f"\tAttaching thing {device_name} and certificate {certificateArn}...")
         
             # Attach policy to things
             logger_aws_iot_core.info(
-                    f"\tAttaching thing {thingNames} and policy {policyName}...")
+                    f"\tAttaching thing {device_name} and policy {policyName}...")
             iot_client.attach_principal_policy(
                 policyName=policyName, principal=certificateArn)
                 
                 
                 
             # pushing meta data to the AWS Dynamodb
-            client = boto3.client('dynamodb')
-            data = client.put_item(
+            
+            data = dynamodb_client.put_item(
               TableName='bick_iot_device_group',
               Item={
                 
